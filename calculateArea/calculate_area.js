9890f82bd6c47811f954b85d19a34fd1eb653898
EzgiLenaS <ezgilena00@gmail.com>
2024-11-11 21:53:23 +0300

    backtick is  ve yapilisi ugrastiriyor onu duzelttim baska bir sey kullanilamaz mi?

--- calculateArea/calculate_area.js
+++ calculateArea/calculate_area.js
@@ -6,7 +6,7 @@ function calculateArea(){
     width = parseFloat(document.getElementById('width').value);
 
     let area = length * width;
-    document.getElementById('result').innerText = 'The area of the rectangle is: ${area}';
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 function groceryTracker(){
     grocery1 = parseFloat(document.getElementById('grocery1').value);
@@ -14,5 +14,5 @@ function groceryTracker(){
     grocery3 = parseFloat(document.getElementById('grocery3').value);
 
     let totalAmount = grocery1 + grocery2 + grocery3;
-    document.getElementById('totalAmount').innerText = 'The total amount is:$ ${totalAmount}';
+    document.getElementById('totalAmount').innerText = `The total amount is:$ ${totalAmount}`;
 }
