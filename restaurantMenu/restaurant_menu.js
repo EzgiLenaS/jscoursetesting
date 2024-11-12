2c3b5aa12c092ea9a4a7270e9cc3b322bec85599
EzgiLenaS <ezgilena00@gmail.com>
2024-11-13 00:59:39 +0300

    Actually I couldnt understand the <div id=breakfast/mainCourse/dessertTotalItems> part but I used map and two types of for each methods in this commit

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,19 @@
+const breakfastMenu =['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu =['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu =['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const prices =['$12', '$22.99', '$21.99', '$15'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}- ${prices[index]}</p>`).join(``);
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+});
+document.getElementById('mainCourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for(let i = 0; i < dessertMenu.length; i++){
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
